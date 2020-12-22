f6cef4f0ec641429af623649a6c0b247858dc75e
Vlad Panteleev <vlad.panteleev@hotmail.com>
2020-12-22 23:04:34 +0300

    Пантелеев В.В. Реализовано домашнее задание №2.

--- src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
+++ src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
@@ -1,47 +1,84 @@
 package ru.digitalhabbits.homework2;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 import static java.lang.Runtime.getRuntime;
 import static java.nio.charset.Charset.defaultCharset;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class FileProcessor {
-    private static final Logger logger = getLogger(FileProcessor.class);
-    public static final int CHUNK_SIZE = 2 * getRuntime().availableProcessors();
+	private static final Logger logger = getLogger(FileProcessor.class);
+	public static final int CHUNK_SIZE = 2 * getRuntime().availableProcessors();
 
-    public void process(@Nonnull String processingFileName, @Nonnull String resultFileName) {
-        checkFileExists(processingFileName);
+	private final LineProcessor<Integer> lineProcessor = new LineCounterProcessor();
 
-        final File file = new File(processingFileName);
-        // TODO: NotImplemented: запускаем FileWriter в отдельном потоке
+	public void process(@Nonnull String processingFileName, @Nonnull String resultFileName) {
+		checkFileExists(processingFileName);
 
-        try (final Scanner scanner = new Scanner(file, defaultCharset())) {
-            while (scanner.hasNext()) {
-                // TODO: NotImplemented: вычитываем CHUNK_SIZE строк для параллельной обработки
+		final File file = new File(processingFileName);
+		var exchanger = new Exchanger<List<Pair<String, Integer>>>();
+		var composeLineExecutorService = Executors.newFixedThreadPool(CHUNK_SIZE);
 
-                // TODO: NotImplemented: обрабатывать строку с помощью LineProcessor. Каждый поток обрабатывает свою строку.
+		var fileWriterThread = new Thread(new FileWriter(new File(resultFileName), exchanger));
+		fileWriterThread.start();
 
-                // TODO: NotImplemented: добавить обработанные данные в результирующий файл
-            }
-        } catch (IOException exception) {
-            logger.error("", exception);
-        }
+		try (final Scanner scanner = new Scanner(file, defaultCharset())) {
+			var futures = new ArrayList<CompletableFuture<Pair<String, Integer>>>();
+			while (scanner.hasNext()) {
+				for (var i = 0; i < CHUNK_SIZE; i++) {
+					var line = scanner.nextLine();
 
-        // TODO: NotImplemented: остановить поток writerThread
+					futures.add(CompletableFuture.supplyAsync(() -> lineProcessor.process(line), composeLineExecutorService));
 
-        logger.info("Finish main thread {}", Thread.currentThread().getName());
-    }
+					if (!scanner.hasNext()) {
+						break;
+					}
+				}
 
-    private void checkFileExists(@Nonnull String fileName) {
-        final File file = new File(fileName);
-        if (!file.exists() || file.isDirectory()) {
-            throw new IllegalArgumentException("File '" + fileName + "' not exists");
-        }
-    }
+				if(!futures.isEmpty()){
+					exchanger.exchange(composeFutureResult(futures).get());
+					futures.clear();
+				}
+			}
+		} catch (IOException  exception) {
+			logger.error("Ошибка чтения файла", exception);
+		} catch (InterruptedException exception) {
+			logger.error("Ошибка потока", exception);
+		} catch (ExecutionException exception) {
+			logger.error("Ошибка ExecutorService", exception);
+		}
+
+		fileWriterThread.interrupt();
+		composeLineExecutorService.shutdown();
+
+		logger.info("Finish main thread {}", Thread.currentThread().getName());
+	}
+
+	private void checkFileExists(@Nonnull String fileName) {
+		final File file = new File(fileName);
+		if (!file.exists() || file.isDirectory()) {
+			throw new IllegalArgumentException("File '" + fileName + "' not exists");
+		}
+	}
+
+	private CompletableFuture<List<Pair<String, Integer>>> composeFutureResult(@Nonnull ArrayList<CompletableFuture<Pair<String, Integer>>> futures) {
+		return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+				.thenApply(unused -> futures.stream()
+						.map(CompletableFuture::join)
+						.collect(Collectors.toList())
+				);
+	}
 }
--- src/main/java/ru/digitalhabbits/homework2/FileWriter.java
+++ src/main/java/ru/digitalhabbits/homework2/FileWriter.java
@@ -1,18 +1,49 @@
 package ru.digitalhabbits.homework2;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.Exchanger;
 
 import static java.lang.Thread.currentThread;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class FileWriter
-        implements Runnable {
-    private static final Logger logger = getLogger(FileWriter.class);
+		implements Runnable {
+	private static final Logger logger = getLogger(FileWriter.class);
 
-    @Override
-    public void run() {
-        logger.info("Started writer thread {}", currentThread().getName());
+	private final File file;
+	private final Exchanger<List<Pair<String, Integer>>> exchanger;
 
-        logger.info("Finish writer thread {}", currentThread().getName());
-    }
+	public FileWriter(File file, Exchanger<List<Pair<String, Integer>>> exchanger) {
+		this.file = file;
+		this.exchanger = exchanger;
+	}
+
+	@Override
+	public void run() {
+		logger.info("Started writer thread {}", currentThread().getName());
+
+		try (java.io.FileWriter fileWriter = new java.io.FileWriter(file, false)) {
+			while (!currentThread().isInterrupted()) {
+				var writeData = exchanger.exchange(null);
+				if (writeData.isEmpty()) {
+					continue;
+				}
+
+				for (var lineData : writeData) {
+					fileWriter.write(String.format("%s %d/n", lineData.getKey(), lineData.getValue()));
+				}
+			}
+		} catch (IOException exception) {
+			logger.error("Ошибка при попытке открыть файл", exception);
+		} catch (InterruptedException exception) {
+			logger.error("Не удалось получить данные от других потов", exception);
+		}
+
+		logger.info("Finish writer thread {}", currentThread().getName());
+	}
 }
